fdcf6216bc667aefd039c83693702b2ddc1b4435
Darren Campbell <darren2802@gmail.com>
2019-09-06 08:52:55 -0600

    Initial commit

---
+++ day_4_methods.rb
@@ -0,0 +1,35 @@
+# In the exercises below, write your own code where indicated
+# to achieve the desired result. You should be able to run this
+# file from your terminal with the command `ruby day_4/exercises/methods.rb`
+
+# example: Write a method below that, when called will print your name
+def print_name
+  p "Severus Snape"
+end
+
+print_name
+
+# Write a method that takes an argument of your name and prints your name
+def print_name(name)
+  p name
+end
+
+print_name("Albus Dumbledore")
+
+# Write a method that takes in 2 numbers and prints their sum, then call that
+# method.
+def print_num(num1, num2)
+  p num1 + num2
+end
+
+print_num(3,4)
+
+# Write a method that takes in two strings and prints a concatenation
+# of those two strings, for example the arguments could be (man, woman) and
+# the end result might be "When Harry Met Sally".  Then, call that method.
+
+def string_thing(str1, str2)
+  p str1 + str2
+end
+
+string_thing("Hello ", "how are you?")
